092df5b98b9b2f5f82d14495b1427897efe73bc5
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2023-03-25 12:34:39 -0300

    fix: chance the user's schema valadatio. add custom error names

--- src/schema/userValidation.schema.ts
+++ src/schema/userValidation.schema.ts
@@ -1,15 +1,15 @@
 import * as yup from "yup"
 import {SchemaOf} from "yup"
-import { iUser_create_request, iUser_update } from "../interface/user.interface"
+import { iUser_create_request, iUser_update_request } from "../interface/user.interface"
 
 export const userValidationCreate: SchemaOf<iUser_create_request> = yup.object().shape({
-    name: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup.string().required(),
-    phone_number: yup.string().required()
+    name: yup.string().required("Name is a required field"),
+    email: yup.string().email().required("Email is a required field"),
+    password: yup.string().required("Password is a required field"),
+    phone_number: yup.string().required("Phone number is a required field")
 })
 
-export const userValidationUpdate: SchemaOf<iUser_update> = yup.object().shape({
+export const userValidationUpdate: SchemaOf<iUser_update_request> = yup.object().shape({
     name: yup.string(),
     email: yup.string().email(),
     password: yup.string(),

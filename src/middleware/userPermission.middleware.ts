8c55ba3776bb481e826eacc198df09e8673aaf49
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2023-03-25 12:37:03 -0300

    add: new condicional to verify if contact existe

--- src/middleware/userPermission.middleware.ts
+++ src/middleware/userPermission.middleware.ts
@@ -8,15 +8,21 @@ const clientPermissionMiddleware = async (req: Request, res: Response, next: Nex
 
     const contactRepository = appDataSource.getRepository(Contacts)
 
+    const is_contact = await contactRepository.findOneBy({id: +req.params.id})
+
+    if(!is_contact){
+        return res.status(404).json("Only who that created the contact is able to update or delete")
+
+    }
+
     const contact_obj: iContacts | null = await contactRepository.createQueryBuilder("Contacts")
     .innerJoinAndSelect("Contacts.user", "user")
-    .where("Contacts.id = :idContact", {idContact: +req.params.id})
+    .where("Contacts.id = :idContact", {idContact: req.params.id})
     .andWhere("user.id = :idUser", {idUser: req.user.id})
     .getOne()
-
-
+    
     if(!contact_obj){
-        throw new AppError("Only who that created the contact is able to update or delete", 401)
+        return res.status(401).json("Only who that created the contact is able to update or delete")
     }
 
     next()

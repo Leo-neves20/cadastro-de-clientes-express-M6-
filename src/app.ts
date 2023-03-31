647f62245b45dc06890506c94df635f10d27e8e5
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2023-03-31 15:16:25 -0300

    feat: created function generate pdf

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors"
 import { handleError } from "./error/appError.error"
 import contactsRoutes from "./routes/contacts.routes"
 import loginRoutes from "./routes/login.routes"
+import pdfRoutes from "./routes/pdf.routes"
 import userRouter from "./routes/user.routes"
 
 export const app = express()
@@ -11,5 +12,6 @@ app.use(express.json())
 app.use("/api/user", userRouter)
 app.use("/api/login", loginRoutes)
 app.use("/api/contact", contactsRoutes)
+app.use("/api/pdf/generate", pdfRoutes)
 
 app.use(handleError)

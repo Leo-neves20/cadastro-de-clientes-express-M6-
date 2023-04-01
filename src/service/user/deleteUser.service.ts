f2e161743552ce842e16cf3b63a4316edfe9e009
Leonardo Neves <101361395+Leo-neves20@users.noreply.github.com>
2023-04-01 01:32:41 -0300

    Feat/pdf (#10)
    * feat: created function generate pdf
    
    * Develop (#9)
    
    * built: built the base of the project
    
    * feat: created user CRUD (#1)
    
    * Feat/contacts (#2)
    
    * feat: created user CRUD
    
    * feat: create contact CRUD
    
    * feat: created login function, fix some ORM's bugs at contacts and created a token validate middleware. (#3)
    
    * Add/schema (#4)
    
    * feat: created login function, fix some ORM's bugs at contacts and created a token validate middleware.
    
    * add: add body validators on routes
    
    * Add/permission (#5)
    
    * feat: created login function, fix some ORM's bugs at contacts and created a token validate middleware.
    
    * add: add body validators on routes
    
    * add: add on routs contact tha validation if the user who is trying to delete or update is the same who created the contact
    
    * Feat/testes (#6)
    
    * feat: created login function, fix some ORM's bugs at contacts and created a token validate middleware.
    
    * add: add body validators on routes
    
    * add: add on routs contact tha validation if the user who is trying to delete or update is the same who created the contact
    
    * fix: change status code if email or password is invalid
    
    * fix: add a condicional to verifi if product exist
    
    * fix: chance the user's schema valadatio. add custom error names
    
    * fix: add the await in validate const that i forgot
    
    * feat: create mock tests
    
    * add: new condicional to verify if contact existe
    
    * add: created a new user interface without password
    
    * fix: takeover the ruler nullable to all fields
    
    * feat: created routes tests
    
    * feat: did the test server configuration
    
    * feat: created function generate pdf (#8)

--- src/service/user/deleteUser.service.ts
+++ src/service/user/deleteUser.service.ts
@@ -1,17 +1,33 @@
 import appDataSource from "../../data-source";
+import Contacts from "../../entities/contacts.entities";
 import User from "../../entities/user.entity";
 import { AppError } from "../../error/appError.error";
 import { iUser } from "../../interface/user.interface";
 
 export const deleteUserService = async (idUser: string): Promise<void> => {
 
-  const userRepository = appDataSource.getRepository(User);
+  const userRepository = appDataSource.getRepository(User); 
+  const contactRepository = appDataSource.getRepository(Contacts)
 
-  const is_user: iUser | null = await userRepository.findOneBy({ id: idUser });
+  const is_user = await userRepository.findOne({
+    where: {
+      id: idUser,
+    },
+    relations: {
+      contacts: true,
+    },
+  });
 
   if (!is_user) {
     throw new AppError("user not found", 404);
   }
+
+  is_user.contacts.forEach(async (contact) => {
+
+    await contactRepository.delete({id: contact.id})
+
+  })
+
 
   await userRepository.delete({ id: idUser });
   

273d75c17afeb2cfce9d4ec1db1c9b66dda76a34
Leonardo Neves <101361395+Leo-neves20@users.noreply.github.com>
2023-04-01 01:33:33 -0300

    Delete pdf.routes.ts

--- src/routes/pdf.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import pdfController from "../controller/pdf.controller";
-import tokenValidationMiddleware from "../middleware/tokenValidation.middleware";
-
-const pdfRoutes = Router()
-
-pdfRoutes.get("", tokenValidationMiddleware, pdfController)
-
-export default pdfRoutes
